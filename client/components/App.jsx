4c46d8105b0adc8a52da2b520f73658c6843e125
magda-gierg <mjgiergiczny@gmail.com>
2017-11-06 14:07:47 +1300

    App component in progress

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import Home from './Home'
+import Menu from './Menu'
+import About from './About'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
-}
+let actions = require('./../actions');
 
-export default App
+let Page = React.createClass ({
 
+  render() {
+    const content = this.props.content;
+    const switchLanguage = this.props.switchLanguage;
+
+    if (content) {
+      return (
+        <div>
+          <Menu data={content.page.menu} switchLanguage={switchLanguage}/>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-10 col-md-offset-1">
+                <Home data={content.page.home}/>
+                <About data={content.page.about}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return;
+    }
+  }
+});
+
+module.exports = ReactRedux.connect(
+  (state) => ({content: state.content}),
+  (dispatch) => ({switchLanguage: (lang) => dispatch(actions.switchLanguage(lang))})
+)(App);
